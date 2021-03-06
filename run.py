e2fd82a9fd75a7f7fcab06f37cdebfbaf9ec8a51
alandoherty95 <alandoherty95@gmail.com>
2021-03-06 14:37:58 +0000

    Initial Commit

---
+++ run.py
@@ -0,0 +1,36 @@
+import os
+import json
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", page_title="Contact")
+
+
+@app.route("/events")
+def events():
+    return render_template("events.html", page_title="Events")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
